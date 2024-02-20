6735a2e23b6ce99315cbf3feb88a981fdd593384
Lowkeyliam <liammaina870@gmail.com>
2024-02-20 13:37:05 +0300

    init

---
+++ budgetapp/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path
+from budgetapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home),
+    path('about/', views.about),
+    path('table/', views.table),
+    path('form/', views.form),
+
+
+
+
+]
